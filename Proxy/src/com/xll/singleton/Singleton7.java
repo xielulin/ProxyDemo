7a45a992bcff1085fa416cfd6a99cab0ac5fa0e5
xielulin <656600528@qq.com>
2019-01-12 09:59:20 +0800

    新增单例模式类

---
+++ Proxy/src/com/xll/singleton/Singleton7.java
@@ -0,0 +1,18 @@
+package com.xll.singleton;
+
+/**
+ * @author xielulin
+ * @create 2019-01-12 09:56
+ * @desc 静态内部类（推荐)
+ **/
+public class Singleton7 {
+    private Singleton7(){}
+
+    private static class SingletonInstance{
+        private static final Singleton7 INSTANCE = new Singleton7();
+    }
+
+    public static Singleton7 getInstance(){
+        return SingletonInstance.INSTANCE;
+    }
+}
